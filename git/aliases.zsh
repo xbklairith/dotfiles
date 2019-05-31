11f446551f6116a61c1c3535da529a5589568880
Praphan 'Bird' Klairith <praphan.k@COH000322.local>
2019-05-31 09:45:24 +0700

    Remove oh-my-zsh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,11 +14,13 @@ alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -m'
+alias gcnc='git commit --no-edit --amend' # commit no comment
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias gcd='git checkout develop'
+alias gcm='git checkout master'
--- oh-my-zsh/install.sh
+++
@@ -1,8 +0,0 @@
-
-export ZSH=~/.oh-my-zsh
-
-if [ ! -d $ZSH ]; then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git $ZSH
-fi
-
-# export ZSH="$HOME/.oh-my-zsh/"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
