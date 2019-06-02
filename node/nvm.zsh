7e66049583ed9f857c952b47183007889d2b11d0
Praphan Klairith <xbird007@gmail.com>
2019-06-02 21:17:47 +0700

    Update load_nvm after node used

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,1 +1,12 @@
 export ME="Hi"
+
+## zsh not load nvm on startup
+function load_nvm {
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
+alias node='unalias nvm; unalias node; unalias npm; load_nvm; node $@'
+alias npm='unalias nvm; unalias node; unalias npm; load_nvm; npm $@'
+alias nvm='unalias nvm; unalias node; unalias npm; load_nvm; nvm $@'
