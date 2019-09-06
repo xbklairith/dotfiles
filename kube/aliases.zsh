2ee8fc8f1ec1235335bd0fee5ec78254f2f34e5c
Praphan Klairith <xbird007@gmail.com>
2019-09-06 10:40:05 +0700

    Update alias

--- kube/aliases.zsh
+++ kube/aliases.zsh
@@ -14,8 +14,7 @@ alias kgetpo="k get po $*"
 alias kgp="k get po $*"
 alias kdelpo="k delete po $*"
 alias kdp="k delete po $*"
+
 alias kscale0="kubectl get statefulsets -o name | xargs -I % kubectl scale % --replicas=0  && kubectl get deployments -o name | xargs -I % kubectl scale % --replicas=0"
-
 alias kscale1="kubectl get statefulsets -o name | xargs -I % kubectl scale % --replicas=1  && kubectl get deployments -o name | xargs -I % kubectl scale % --replicas=1"
-
 alias knuke='kn | grep pdm | while read namespace; do kn "$namespace" && kscale0 ; done' 
