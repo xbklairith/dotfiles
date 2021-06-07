60087ba95ccfecc7950018bb0f12c644e7d8ef6a
Bird Praphan <xbird007@gmail.com>
2021-06-07 16:25:39 +0700

    Update yarn npx onload

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -3,18 +3,17 @@ export ME="Hi"
 ## zsh not load nvm on startup
 function load_nvm {
   export NVM_DIR="$HOME/.nvm"
-  if [ "$(uname -s)" = "Darwin" ]
-  then
-    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  if [ "$(uname -s)" = "Darwin" ]; then
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
   else
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
   fi
 }
 
-alias node='unalias nvm; unalias node; unalias npm; load_nvm; node $@'
-alias npm='unalias nvm; unalias node; unalias npm; load_nvm; npm $@'
-alias nvm='unalias nvm; unalias node; unalias npm; load_nvm; nvm $@'
-alias yarn='unalias nvm; unalias node; unalias npm; load_nvm; yarn $@'
-alias npx='unalias nvm; unalias node; unalias npm; load_nvm; npx $@'
+alias node='unalias nvm; unalias node; unalias npm; unalias yarn ; unalias npx; node $@'
+alias npm='unalias nvm; unalias node; unalias npm; unalias yarn ; unalias npx; npm $@'
+alias nvm='unalias nvm; unalias node; unalias npm; unalias yarn ; unalias npx; nvm $@'
+alias yarn='unalias nvm; unalias node; unalias npm; unalias yarn ; unalias npx; yarn $@'
+alias npx='unalias nvm; unalias node; unalias npm; unalias yarn ; unalias npx; load_nvm; npx $@'
